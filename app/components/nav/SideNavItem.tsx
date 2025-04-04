1d2c6ad8de8137c20c5d82fb4dfdf6169e67bffc
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-04 21:47:36 +0330

    feat (app/components/nav/SideNavItem): Create side navigation item component with icon and hover effect

---
+++ app/components/nav/SideNavItem.tsx
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+type SideNavItemProps = {
+  title: string;
+  icon: IconDefinition; // React element type
+  linkTo: string;
+};
+
+export default function SideNavItem({ icon, title, linkTo }: SideNavItemProps) {
+  return (
+    <li
+      className={`mb-4 text-xl hover:opacity-90 flex justify-start items-center gap-4 text-slate-50`}
+    >
+      <FontAwesomeIcon icon={icon} className="text-base" />
+      <a href={linkTo}>{title}</a>
+    </li>
+  );
+}
