91edbac289f37dd4803481aac01d414ee2c032fd
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-04 21:35:28 +0330

    feat (app/layout.tsx): Add Montserrat font with specified weights for global usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Montserrat } from "next/font/google";
+import "./lib/fontawesome";
+const montserrat = Montserrat({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Job Board app",
   description: "Search and find the job that fits your life",
@@ -24,11 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${montserrat.className}`}>{children}</body>
     </html>
   );
 }
