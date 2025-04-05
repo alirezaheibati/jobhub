78f7f67fc669ab2152af8261e4014ed569d1ee23
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-05 20:19:15 +0330

    chore (tailwind.config.ts): Add custom colors for as primary and secondary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#181255",
+        secondary: "#FF9D3D",
       },
     },
     screens: {
