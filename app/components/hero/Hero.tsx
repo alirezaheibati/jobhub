afa8cd784df300ac538d4e4ed2f6efe02e0b7865
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-05 20:29:46 +0330

    feat (app/components/hero): Create Hero component as container for hero section

---
+++ app/components/hero/Hero.tsx
@@ -0,0 +1,12 @@
+import HeroBtn from "./HeroBtn";
+import HeroContent from "./HeroContent";
+import HeroImage from "./HeroImage";
+
+export default function Hero() {
+  return (
+    <section className="bg-slate-100 flex justify-start items-end flex-col-reverse lg:flex-row w-full h-[calc(100vh-64px)] md:h-[calc(100vh-106px)]">
+      <HeroImage />
+      <HeroContent />
+    </section>
+  );
+}
