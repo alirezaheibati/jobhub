9a31996d8a2921830e1051d033ba772e230837a0
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-05 20:30:20 +0330

    feat (app/page.tsx): Add Hero component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "./components/hero/Hero";
 import TopNav from "./components/nav/TopNav";
 
 export default function Home() {
   return (
     <div>
       <TopNav />
+      <Hero />
     </div>
   );
 }
