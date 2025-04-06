abdbc9cac7d4631f8371dbf5885ae04d339ca5b0
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-06 20:41:25 +0330

    refactor (app/components/hero/HeroImage): Replace Next Image component with img tag

--- app/components/hero/HeroImage.tsx
+++ app/components/hero/HeroImage.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import heroImg from "../../assets/images/job-bg.png";
 export default function HeroImage() {
   return (
     <div className="lg:w-3/5 w-full sm:w-4/5 md:w-2/3 block mx-auto max-w-[800px]">
-      <img src={heroImg.src} alt="Hero image" className="w-full" />
+      <Image src={heroImg} alt="Hero image" className="w-full" />
     </div>
   );
 }
