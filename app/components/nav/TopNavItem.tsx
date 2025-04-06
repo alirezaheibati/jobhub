7e41e310ca9ff15c9efd46b80d308d01477565e4
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-06 20:37:51 +0330

    refactor (app/components/nav/TopNavItem): Replace Next Link component with a tag

--- app/components/nav/TopNavItem.tsx
+++ app/components/nav/TopNavItem.tsx
@@ -1,5 +1,6 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 type TopNavItemProps = {
   title: string;
@@ -10,14 +11,14 @@ type TopNavItemProps = {
 export default function TopNavItem({ title, icon, linkTo }: TopNavItemProps) {
   return (
     <li className={`group px-3 text-slate-700 font-[600]`}>
-      <a href={linkTo}>
+      <Link href={linkTo}>
         <p>
           <FontAwesomeIcon icon={icon} className="text-sm mb-[2px] mr-1" />
           {title}
         </p>
 
         <div className="w-0 bg-slate-700 h-[2px] group-hover:w-full transition-width duration-300 ease-in-out"></div>
-      </a>
+      </Link>
     </li>
   );
 }
