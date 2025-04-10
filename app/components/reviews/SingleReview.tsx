b284e4aad3f4d645d4ffc5a548df8f0a609c35d1
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-10 14:51:49 +0330

    feat (app/components/reviews): Create SingleReview component for displaying individual reviews

---
+++ app/components/reviews/SingleReview.tsx
@@ -0,0 +1,36 @@
+import ReviewInterface from "@/app/interface/ReviewInterface";
+
+interface SingleReviewProps {
+  activeStatus: boolean;
+  review: ReviewInterface;
+}
+export default function SingleReview({
+  activeStatus,
+  review,
+}: SingleReviewProps) {
+  return (
+    <div
+      className={`border-2 border-primary rounded-xl p-4 flex flex-col justify-between items-start transition-all duration-300 w-[80vw] md:w-[30vw] ${
+        activeStatus ? "bg-sky-50 scale-110 md:scale-[1.15]" : " scale-100"
+      }`}
+    >
+      <div>
+        <div className="h-12 w-12 rounded-full border-2 p-2">
+          <img src={review.logo} alt={review.occupation} className="w-full" />
+        </div>
+        <p className={`my-3 line-clamp-4 `}>{review.review}</p>
+      </div>
+      <div className="flex justify-start items-center gap-2">
+        <img
+          src={review.avatar}
+          alt={review.writer}
+          className="h-14 w-14 rounded-full border-2"
+        />
+        <div className="flex flex-col justify-start items-start">
+          <p className="font-semibold md:text-lg">{review.writer}</p>
+          <p className="text-sm line-clamp-1">{review.occupation}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
