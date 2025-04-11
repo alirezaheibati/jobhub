f86dec9da2455776a6ea5304912010cad452b333
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-04-11 17:30:21 +0330

    feat ( app/interface): Create FeaturedJobInterface to define structure for featured job data

---
+++ app/interface/FeaturedJobInterface.ts
@@ -0,0 +1,10 @@
+export default interface FeaturedJobInterface {
+  id: number;
+  title: string;
+  salary: string;
+  shift: string;
+  company: string;
+  logo: string;
+  location: string;
+  skills: string;
+}
