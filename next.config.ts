181153c48762b185bd33fb883f228ca30e0d0906
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-05-02 21:33:06 +0330

    chore (next.config.ts): Update next.config.js: Allow images from alirezaheibati.ir

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/projects/jobhob",
+  images: {
+    domains: ["alirezaheibati.ir"], // Add the domain hosting the image
+  },
 };
 
 export default nextConfig;
