8a326770ec66e9126d96eba8c05bdb94b1e4b858
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-05-03 18:59:19 +0330

    feat (app/page.tsx): Render essential components in Home page for structured layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 import Brands from "./components/brands/Brands";
 import FeaturedJobs from "./components/featured-job/FearuredJobs";
+import Features from "./components/features/Features";
+import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
 import TopNav from "./components/nav/TopNav";
+import PopularContainer from "./components/popular-cats/PopularsContainer";
 import ReviewsWrapper from "./components/reviews/ReviewsWrapper";
 
 export default function Home() {
   return (
     <div>
-      <TopNav />
       <Hero />
       <Brands />
-      <ReviewsWrapper />
+      <Features />
       <FeaturedJobs />
+      <ReviewsWrapper />
+      <PopularContainer />
+      <Footer />
     </div>
   );
 }
