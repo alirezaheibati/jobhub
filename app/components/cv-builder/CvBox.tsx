d32414d1c31bf5e43e9d916d4f37b38df9e8cb2b
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-05-05 18:31:55 +0330

    feat (app/components/cv-builder/CvBox): add  a header div element in CvBox to label each resume section with title and icon

--- app/components/cv-builder/CvBox.tsx
+++ app/components/cv-builder/CvBox.tsx
@@ -1,13 +1,20 @@
-import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faPen, faUserTie } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 interface CvBoxProps {
   children: ReactNode;
+  title: string;
+  icon: IconProp;
 }
-export default function CvBox({ children }: CvBoxProps) {
+export default function CvBox({ children, title, icon }: CvBoxProps) {
   return (
-    <section className=" border border-primary border-l-8 p-8 relative rounded-lg group shadow-lg mb-6 last:mb-0">
-      <button className="text-secondary text-lg font-semibold justify-start items-center gap-1 absolute right-4 top-4 hidden group-hover:flex">
+    <section className=" border border-primary border-l-8 pb-8 px-2 pt-24 md:px-8 relative rounded-lg group shadow-lg mb-6 last:mb-0 overflow-hidden">
+      <div className="flex justify-start items-center gap-2 bg-slate-100 absolute left-0 top-0 right-0 pl-8 py-3">
+        <FontAwesomeIcon icon={icon} />
+        <h2 className="font-semibold">{title}</h2>
+      </div>
+      <button className="text-secondary text-lg font-semibold justify-start items-center gap-1 absolute right-4 top-14 hidden group-hover:flex">
         <FontAwesomeIcon icon={faPen} />
         Edit
       </button>
