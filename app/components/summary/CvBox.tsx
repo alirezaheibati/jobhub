a1ac9e8e84a66b6b303519b94c8c314123c99b86
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-05-06 10:36:19 +0330

    feat (app/components/summary/CvBox): add editMode and onActiveEditMode props to conditionally render edit button and use button to activate edit mode

--- app/components/summary/CvBox.tsx
+++ app/components/summary/CvBox.tsx
@@ -6,18 +6,31 @@ interface CvBoxProps {
   children: ReactNode;
   title: string;
   icon: IconProp;
+  editMode: boolean;
+  onActiveEditMode: () => void;
 }
-export default function CvBox({ children, title, icon }: CvBoxProps) {
+export default function CvBox({
+  children,
+  title,
+  icon,
+  editMode,
+  onActiveEditMode,
+}: CvBoxProps) {
   return (
     <section className=" border border-primary border-l-8 pb-8 px-2 pt-24 md:px-8 relative rounded-lg group shadow-lg mb-6 last:mb-0 overflow-hidden">
       <div className="flex justify-start items-center gap-2 bg-slate-100 absolute left-0 top-0 right-0 pl-8 py-3">
         <FontAwesomeIcon icon={icon} />
         <h2 className="font-semibold">{title}</h2>
       </div>
-      <button className="text-secondary text-lg font-semibold justify-start items-center gap-1 absolute right-4 top-14 hidden group-hover:flex">
-        <FontAwesomeIcon icon={faPen} />
-        Edit
-      </button>
+      {editMode && (
+        <button
+          className="text-secondary text-lg font-semibold justify-start items-center gap-1 absolute right-4 top-14 hidden group-hover:flex"
+          onClick={onActiveEditMode}
+        >
+          <FontAwesomeIcon icon={faPen} />
+          Edit
+        </button>
+      )}
       {children}
     </section>
   );
